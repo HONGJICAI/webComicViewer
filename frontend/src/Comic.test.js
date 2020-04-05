a3ed23f4f1bc8e7f1a5ef63e0a5331c9eeeea447
hongjicai <c464580613@gmail.com>
2020-04-05 22:09:14 +0800

    upgrade react-scripts to 3.0.0

---
+++ frontend/src/Comic.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import Comic from './Comic';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Router><Comic /></Router>, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
